507ee7bf9441aa0bd2b1c1ab5a26002eeec0a0ed
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-22 19:28:13 -0600

    trying to see if everything working

--- create/create.js
+++ create/create.js
@@ -1,41 +1,43 @@
-import { 
-    createBunny, 
-    getFamilies, 
-    checkAuth, 
-    logout 
+import {
+    createBunny,
+    getFamilies,
+    checkAuth,
+    logout
 } from '../fetch-utils.js';
-
-const form = document.querySelector('.bunny-form');
-const logoutButton = document.getElementById('logout');
-
-form.addEventListener('submit', async e => {
-    // prevent default
-
-    // get the name and family id from the form
-
-    // use createBunny to create a bunny with this name and family id
-    
-    form.reset();
-});
-
-window.addEventListener('load', async() => {
-    // let's dynamically fill in the families dropdown from supabase
-    // grab the select HTML element from the DOM
-
-    // go get the families from supabase
-
-    // for each family
-
-    // create an option tag
-
-    // set the option's value and text content
-
-    // and append the option to the select
-});
-
-
 checkAuth();
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+const form = document.querySelector('.bunny-form');
+const logoutButton = document.getElementById('logout');
+
+form.addEventListener('submit', async (e) => {
+    // prevent default
+    e.preventDefault();
+    // get the name and family id from the form
+    const newForm = new FormData(form);
+    const bunny = ({ name: newForm.get('bunny-name'), family_id: newForm.get('family-id') });
+    //console.log(bunny);
+    // use createBunny to create a bunny with this name and family id
+    await createBunny(bunny);
+    form.reset();
+});
+
+window.addEventListener('load', async () => {
+    // let's dynamically fill in the families dropdown from supabase
+    // grab the select HTML element from the DOM
+    const select = document.getElementById('select');
+    // go get the families from supabase
+    const families = await getFamilies();
+    // for each family
+    for (let family of families) {
+        // create an option tag
+        const option = document.createElement('option');
+        // set the option's value and text content
+        option.textContent = family.name;
+        option.value = family.id;
+        // and append the option to the select
+        select.append(option);
+    }
 });
